d4d93e3f87a275d2a47625d5a0cfc4ad87d49c08
Boasbabs <babalolasimeon@gmail.com>
2016-05-17 19:24:18 +0530

    Add user model using Devise

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'home#land'
 
+  devise_for :users
+  
   get "translate" => "home#translate", as: :translate
 
   resources :books do
